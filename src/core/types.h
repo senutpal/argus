8533581ab53fbbdb2568bf762b874a58df7d496b
Utpal Sen <contactutpalsen@gmail.com>
2026-08-19 16:00:00 +0530

    feat(core): add order status, symbol and comparator

--- src/core/types.h
+++ src/core/types.h
@@ -15,6 +15,30 @@ using SequenceNumber = std::uint64_t;
 using Timestamp = std::uint64_t;
 using ParticipantId = std::uint32_t;
 using MatchId = std::uint64_t;
+using Symbol = std::array<char, 8>;
+
+constexpr Symbol make_symbol(std::string_view str) noexcept {
+    Symbol sym{};
+    const std::size_t len = std::min(str.size(), sym.size());
+    for (std::size_t i = 0; i < len; ++i) {
+        sym[i] = str[i];
+    }
+    return sym;
+}
+
+constexpr std::string_view symbol_view(const Symbol& sym) noexcept {
+    std::size_t len = 0;
+    while (len < sym.size() && sym[len] != '\0') {
+        ++len;
+    }
+    return std::string_view{sym.data(), len};
+}
+
+struct SymbolLess {
+    constexpr bool operator()(const Symbol& lhs, const Symbol& rhs) const noexcept {
+        return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+    }
+};
 
 inline constexpr Price PRICE_SCALE = 10000;
 
@@ -34,5 +58,25 @@ enum class OrderType : std::uint8_t {
     StopLimit,
     Iceberg
 };
+
+enum class OrderStatus : std::uint8_t {
+    New,
+    Accepted,
+    PartiallyFilled,
+    Filled,
+    Cancelled
+};
+
+static_assert(std::is_trivially_copyable_v<Price>);
+static_assert(std::is_trivially_copyable_v<Quantity>);
+static_assert(std::is_trivially_copyable_v<OrderId>);
+static_assert(std::is_trivially_copyable_v<SequenceNumber>);
+static_assert(std::is_trivially_copyable_v<Timestamp>);
+static_assert(std::is_trivially_copyable_v<ParticipantId>);
+static_assert(std::is_trivially_copyable_v<MatchId>);
+static_assert(std::is_trivially_copyable_v<Symbol>);
+static_assert(std::is_trivially_copyable_v<Side>);
+static_assert(std::is_trivially_copyable_v<OrderType>);
+static_assert(std::is_trivially_copyable_v<OrderStatus>);
 
 }
